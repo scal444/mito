f9ad76172a16e84bd6a173a827ba503bf82d1e78
Kevin Boyd <kevin.boyd@uconn.edu>
2018-09-17 11:32:00 -0400

    haven't commited in a while.
    Chalk up another subproject to bad programming practices

--- analysis/assign_frozen_dummy_geometry.py
+++ analysis/assign_frozen_dummy_geometry.py
@@ -3,55 +3,199 @@ import mdtraj as md
 import KB_python.file_io as file_io
 import KB_python.coordinate_manipulation.periodic as periodic
 import KB_python.coordinate_manipulation.transformations as transformations
+import matplotlib.pyplot as plt
+import pickle
 
 import geometry
 
-
-class dummy_sectional_indices:
-    def __init__(self, inner_cylinder, outer_cylinder, inner_junction, outer_junction, inner_flat, outer_flat):
-        self.inner_cylinder = inner_cylinder
-        self.outer_cylinder = outer_cylinder
-        self.inner_junction = inner_junction
-        self.outer_junction = outer_junction
-        self.inner_flat     = inner_flat
-        self.outer_flat     = outer_flat
+# --------------------------------------------------------------------------------------------------------------------
+# pickle utilities
+# --------------------------------------------------------------------------------------------------------------------
 
 
-def unified_to_sectioned():
-    ''' Takes in unified coordinate, outputs values of sectioned and an index of section'''
-    pass
+def pickle_save(obj, path):
+    with open(path, 'wb') as f:
+        pickle.dump(obj, f)
 
 
-def sectioned_to_unified():
-    pass
+def pickle_load(path):
+    with open(path, 'rb') as f:
+        return pickle.load(f)
 
-
-def assign_dummy_particles_to_section(rho, z, top_ind, bot_ind, mito_dims):
-    in_cylinder, in_flat, in_junction = geometry.assign_to_mito_section(rho, z, mito_dims)
-    return dummy_sectional_indices(np.intersect1d(in_cylinder, top_ind), np.intersect1d(in_cylinder, bot_ind),
-                                   np.intersect1d(in_junction, top_ind), np.intersect1d(in_junction, bot_ind),
-                                   np.intersect1d(in_flat,     top_ind), np.intersect1d(in_flat,     bot_ind))
+# ---------------------------------------------------------------------------------------------------------------------
+# index loading and processing
+# ---------------------------------------------------------------------------------------------------------------------
 
 
 def load_dummy_indices(file):
-    indices = file_io.load_gromacs_index('/home/kevin/hdd/Projects/mito/simulations/dummy/POPC_100/index.ndx')
-    top_dummy_ind = np.array(indices['top_DUMY']) - indices['top_DUMY'][0]
-    bot_dummy_ind = np.array(indices['bot_DUMY']) - indices['top_DUMY'][0]  # subtract from top! that's the 0 index
-    return top_dummy_ind, bot_dummy_ind
+    ''' Assumes that bottom dummy section follows top dummy section immediately, and restarts numbering from 0'''
+    indices = file_io.load_gromacs_index(file)
+    inner_dummy_ind = np.array(indices['top_DUMY']) - indices['top_DUMY'][0]
+    outer_dummy_ind = np.array(indices['bot_DUMY']) - indices['top_DUMY'][0]  # subtract from top! that's the 0 index
+    return inner_dummy_ind, outer_dummy_ind
 
 
-def cart_2_mito(dummy_pdb, mito_shape):
-    mito_center = geometry.get_mito_center(dummy_pdb.xyz.squeeze(), mito_shape)
-    mito_center_scaled = mito_center[np.newaxis, :].repeat(dummy_pdb.xyz.shape[1], axis=0)[np.newaxis, :, :]
-    mito_vecs   = periodic.calc_vectors(mito_center_scaled, dummy_pdb.xyz, dummy_pdb.unitcell_lengths)
+def load_leaflet_indices():
+    pass
+
+
+class mito_sectional_indices:
+    def __init__(self, cylinder, junction,  flat):
+        self.cylinder = cylinder
+        self.junction = junction
+        self.flat     = flat
+
+
+def assign_dummy_particles_to_section(rho, z, mito_dims):
+    in_cylinder, in_junction, in_flat = geometry.assign_to_mito_section(rho, z, mito_dims)
+    return mito_sectional_indices(in_cylinder, in_junction, in_flat)
+
+
+def load_lipid_indices(file, lipid_names):
+    pass
+
+# ---------------------------------------------------------------------------------------------------------------------
+# load and process forces
+# ---------------------------------------------------------------------------------------------------------------------
+
+
+def load_raw_forces(path):
+    return file_io.xvg_2_coords(file_io.load_large_text_file(path), 3)
+
+
+# ----------------------------------------------------------------------------------------------------------------------
+# mito coordinate loading
+# ---------------------------------------------------------------------------------------------------------------------
+
+
+class mito_coordinates:
+    ''' Compiled coordinate description of mito system. Can be used for dummy or lipid data'''
+    def __init__(self, theta, rho, z, unified):
+        self.theta = theta
+        self.rho = rho
+        self.z = z
+        self.unified = unified
+
+
+class dummy_leaflet_data:
+    def __init__(self, mito_coordinates, mito_shape, forces=None):
+        # instance of mito_coordinates
+        self.coordinates = mito_coordinates
+        # instance of mito_shape
+        self.mito_shape = mito_shape
+        # n_frames * nparts * 3 array
+        if forces:
+            self.forces = forces
+
+
+class lipid_leaflet_data:
+    def __init__(self, mito_coordinates, lipid_indices, mito_shape):
+        # instance of mito_coordinates
+        self.coordinates = mito_coordinates
+        # dict of indices
+        self.lipid_indices = lipid_indices
+        # instance of mito_shape
+        self.mito_shape = mito_shape
+
+
+def cart_2_mito(coords, unitcell_lengths, mito_center):
+    '''
+        Does a cartesian to polar transformation on trajectory data, based on a given center point. Accounts for periodic
+        boundaries by calling periodic.calc_vectors
+
+    '''
+    mito_center_scaled = mito_center[np.newaxis, :].repeat(coords.shape[1], axis=0)[np.newaxis, :, :]
+    mito_vecs   = periodic.calc_vectors(mito_center_scaled, coords, unitcell_lengths)
     return transformations.cart2pol(mito_vecs.squeeze())
 
 
-def load_dummy_data(forcepath='dummy_forces.xvg', coordpath='dummy_coords.pdb', indexpath='index.ndx'):
-    dummy_forces = file_io.xvg_2_coords(file_io.load_large_text_file(forcepath), 3)
-    dummy_pdb   = md.load(coordpath)
-    top_dummy_ind, bot_dummy_ind = load_dummy_indices(indexpath)
-    return dummy_forces, dummy_pdb, top_dummy_ind, bot_dummy_ind
+def load_and_split_dummy_pdb(pdbpath, indexpath, mito_shape, zo):
+    ''' Analyses should be split by leaflet, but it's easier with current setup to load the whole dummy system and then
+        split into 2 leaflet data structures
+    '''
+    outer_mito_shape = geometry.mito_dims(mito_shape.l_cylinder,      mito_shape.r_cylinder + zo,
+                                          mito_shape.r_junction - zo, mito_shape.l_flat)
+    inner_mito_shape = geometry.mito_dims(mito_shape.l_cylinder,      mito_shape.r_cylinder - zo,
+                                          mito_shape.r_junction + zo, mito_shape.l_flat)
+    dummy_pdb = md.load(pdbpath)
+    dummy_inner, dummy_outer = load_dummy_indices(indexpath)  # these are for dummy-only system, start at 0
+
+    # center and transform whole system
+    center = geometry.get_mito_center(dummy_pdb.xyz.squeeze(), mito_shape.l_cylinder)  # don't need inner/outer, this only uses l_cylinder
+    theta, rho, z = cart_2_mito(dummy_pdb.xyz, dummy_pdb.unitcell_lengths, center)
+
+    # unified coordinate is based on shape, so do separately
+    inner_unified_coord = geometry.map_to_unified_coordinate(z[dummy_inner], rho[dummy_inner], inner_mito_shape)
+    outer_unified_coord = geometry.map_to_unified_coordinate(z[dummy_outer], rho[dummy_outer], outer_mito_shape)
+
+    inner_mito_coordinates =  mito_coordinates(theta.squeeze()[dummy_inner], rho.squeeze()[dummy_inner], z.squeeze()[dummy_inner], inner_unified_coord)
+    outer_mito_coordinates =  mito_coordinates(theta.squeeze()[dummy_outer], rho.squeeze()[dummy_outer], z.squeeze()[dummy_outer], outer_unified_coord)
+    return inner_mito_coordinates, outer_mito_coordinates
+
+
+def load_and_split_dummy_forces(forcepath, indexpath):
+    dummy_forces = load_raw_forces(forcepath)
+    dummy_inner, dummy_outer = load_dummy_indices(indexpath)
+    return dummy_forces[:, dummy_inner, :], dummy_forces[:, dummy_inner, dummy_outer]
+
+
+def load_all_dummy_info(pdbpath, forcepath, indexpath, mito_shape, zo):
+    inner_coords, outer_coords = load_and_split_dummy_pdb()
+
+
+
+def load_mito_traj(trajpath, pdbpath, mito_shape, center):
+    traj = md.load(trajpath, top=pdbpath)
+    theta, rho, z = cart_2_mito(traj.xyz, traj.unitcell_lengths, mito_shape, center)
+    unified_coord = geometry.map_to_unified_coordinate(z, rho, mito_shape)
+    return mito_coordinates(theta, rho, z, unified_coord)
+
+
+def process_dummy_leaflet_forces(unified_coord, forces, bins):
+    force_avg_per_bead = np.sqrt((forces.mean(axis=0) ** 2).sum(axis=1))
+    bin_assignments = np.digitize(unified_coord, bins)
+    forces_by_bin = np.zeros(len(bins) - 1)
+    forces_errors_by_bin   = np.zeros(len(bins) - 1)
+    for bin_ind in range(1, len(bins)):
+        force_ind = bin_ind - 1
+        forces_by_bin[force_ind] = force_avg_per_bead[bin_assignments == bin_ind].mean()
+        forces_errors_by_bin[force_ind] = force_avg_per_bead[bin_assignments == bin_ind].std() / np.sqrt(force_avg_per_bead[bin_assignments == bin_ind].size)
+    return forces_by_bin, forces_errors_by_bin
+
+
+def process_dummy_system(path, dummy_thickness, mito_shape, forcepath='/force.xvg', indexpath='/index.ndx',
+                         coordpath='/dummy_only.pdb', firstframe=0):
+
+    dummy_forces, dummy_pdb, top_dummy_ind, bot_dummy_ind = load_dummy_data(forcepath=path + forcepath,
+                                                                            coordpath=path + coordpath,
+                                                                            indexpath=path + indexpath)
+    outer_mito_shape = geometry.mito_dims(mito_shape.l_cylinder,                   mito_shape.r_cylinder + dummy_thickness,
+                                          mito_shape.r_junction - dummy_thickness, mito_shape.l_flat)
+    inner_mito_shape = geometry.mito_dims(mito_shape.l_cylinder,                   mito_shape.r_cylinder - dummy_thickness,
+                                          mito_shape.r_junction + dummy_thickness, mito_shape.l_flat)
+    theta, rho, z = cart_2_mito(dummy_pdb, mito_shape)
+    partitioned_dummy_indices = assign_dummy_particles_to_section(rho, z, top_dummy_ind, bot_dummy_ind, mito_shape)
+    outer_unified_coord = geometry.map_to_unified_coordinate(z[bot_dummy_ind], rho[bot_dummy_ind], outer_mito_shape)
+    inner_unified_coord = geometry.map_to_unified_coordinate(z[top_dummy_ind], rho[top_dummy_ind], inner_mito_shape)
+
+    bin_spacing = 1
+    outer_bins = np.arange(0, outer_unified_coord.max() + bin_spacing / 2, bin_spacing)
+    inner_bins = np.arange(0, inner_unified_coord.max() + bin_spacing / 2, bin_spacing)
+    outer_bin_centers = (outer_bins[1:] + outer_bins[:-1]) / 2
+    inner_bin_centers = (inner_bins[1:] + inner_bins[:-1]) / 2
+
+    inner_force_means, inner_force_errors = process_dummy_leaflet_forces(inner_unified_coord,
+                                                                         dummy_forces[firstframe:, partitioned_dummy_indices.inner, :],
+                                                                         inner_bins)
+    outer_force_means, outer_force_errors = process_dummy_leaflet_forces(outer_unified_coord,
+                                                                         dummy_forces[firstframe:, partitioned_dummy_indices.outer, :],
+                                                                         outer_bins)
+
+    outer_rho = [geometry.unified_to_rho(outer_mito_shape, i) for i in outer_bin_centers]
+    inner_rho = [geometry.unified_to_rho(inner_mito_shape, i) for i in inner_bin_centers]
+    outer_z = [geometry.unified_to_z(outer_mito_shape, i) for i in outer_bin_centers]
+    inner_z = [geometry.unified_to_z(inner_mito_shape, i) for i in inner_bin_centers]
+    return dummy_data(inner_rho, inner_z, inner_unified_coord, inner_force_means, outer_rho, outer_z, outer_unified_coord, outer_force_means )
 
 
 if __name__ == '__main__':
@@ -61,61 +205,81 @@ if __name__ == '__main__':
     intermediate_data_dir = top_dir + 'data/'
     raw_data_dir          = top_dir + 'simulations/'
 
+    firstframe = 1000
     # mito geometry info
+    dummy_zo = 6.5 / 2
     mito_shape = geometry.mito_dims(30, 10, 10, 56)
+    inner_mito_shape = geometry.dims(30, 10 - dummy_zo, 10 + dummy_zo, 56)
+    outer_mito_shape = geometry.dims(30, 10 + dummy_zo, 10 - dummy_zo, 56)
 
-    # example data setup
-    dummy_forces_path = raw_data_dir + 'frozen_dummy/forces.xvg'
-    dummy_coord_path  = raw_data_dir + 'dummy/POPC_100/dummy_only.pdb'
-    dummy_index_path  = raw_data_dir + 'dummy/POPC_100/index.ndx'
-
-    # load data
-    dummy_forces, dummy_pdb, top_dummy_ind, bot_dummy_ind = load_dummy_data(forcepath=dummy_forces_path,
-                                                                            coordpath=dummy_coord_path,
-                                                                            indexpath=dummy_index_path)
-
+    top_dummy_ind, bot_dummy_ind = load_dummy_indices(dummy_index_path)
     # get center, calculate vectors from center
     theta, rho, z = cart_2_mito(dummy_pdb, mito_shape)
-
-
     partitioned_dummy_indices = assign_dummy_particles_to_section(rho, z, top_dummy_ind, bot_dummy_ind, mito_shape)
-    outer_cyl_coord = geometry.map_to_cyl_regime(z[partitioned_dummy_indices.outer_cylinder], mito_shape)
-    outer_junc_coord = geometry.map_to_junction_regime(z[partitioned_dummy_indices.outer_junction],
-                                                       rho[partitioned_dummy_indices.outer_junction], mito_shape)
-    outer_flat_coord = geometry.map_to_flat_regime(rho[partitioned_dummy_indices.outer_flat], mito_shape)
+    top_unified_coord = geometry.map_to_unified_coordinate(z[top_dummy_ind], rho[top_dummy_ind], top_mito_shape)
+    bot_unified_coord = geometry.map_to_unified_coordinate(z[bot_dummy_ind], rho[bot_dummy_ind], bot_mito_shape)
 
-    outer_cyl_force  = np.sqrt((dummy_forces[:, partitioned_dummy_indices.outer_cylinder, :] ** 2).sum(axis=2)).mean(axis=0)  # noqa
-    outer_junc_force = np.sqrt((dummy_forces[:, partitioned_dummy_indices.outer_junction, :] ** 2).sum(axis=2)).mean(axis=0)  # noqa
-    outer_flat_force = np.sqrt((dummy_forces[:, partitioned_dummy_indices.outer_flat, :] ** 2).sum(axis=2)).mean(axis=0)
+    bin_spacing = 1
+    top_bins = np.arange(0, top_unified_coord.max() + bin_spacing / 2, bin_spacing)
+    bot_bins = np.arange(0, bot_unified_coord.max() + bin_spacing / 2, bin_spacing)
+    top_bin_centers = (top_bins[1:] + top_bins[:-1]) / 2
+    bot_bin_centers = (bot_bins[1:] + bot_bins[:-1]) / 2
+    # double areas to account for up/down
+    top_bin_areas = 2 * np.array([geometry.calc_unified_section_area(top_mito_shape, i, i + 1) for i in top_bins[:-1]])
+    bot_bin_areas = 2 * np.array([geometry.calc_unified_section_area(bot_mito_shape, i, i + 1) for i in bot_bins[:-1]])
+    top_bin_counts = np.bincount(np.digitize(top_unified_coord, top_bins))[1:]
+    bot_bin_counts = np.bincount(np.digitize(bot_unified_coord, bot_bins))[1:]
 
-    bin_spacing = 0.5
-    outer_cyl_bins = np.arange(0, mito_shape.l_cylinder / 2, 0.5)
-    outer_cyl_bin_assignments = np.digitize(outer_cyl_coord, outer_cyl_bins)
-    outer_cyl_bin_centers = (outer_cyl_bins[1:] + outer_cyl_bins[:-1]) / 2
+    inner_force_means, inner_force_errors = process_dummy_leaflet_forces(top_unified_coord,
+                                                                         dummy_forces[firstframe:, partitioned_dummy_indices.inner, :],
+                                                                         top_bins)
+    outer_force_means, outer_force_errors = process_dummy_leaflet_forces(bot_unified_coord,
+                                                                         dummy_forces[firstframe:, partitioned_dummy_indices.outer, :],
+                                                                         bot_bins)
 
-    junc_bin_spacing = (180 / np.pi ) * 0.5 / mito_shape.r_junction
-    outer_junc_bins = np.arange(0, 90, junc_bin_spacing)
-    outer_junc_bin_assignments = np.digitize(outer_junc_coord, outer_junc_bins)
-    outer_junc_bin_centers = (outer_junc_bins[1:] + outer_junc_bins[:-1]) / 2
+    top_rho = [geometry.unified_to_rho(top_mito_shape, i) for i in top_bin_centers]
+    bot_rho = [geometry.unified_to_rho(bot_mito_shape, i) for i in bot_bin_centers]
+    top_z = [geometry.unified_to_z(top_mito_shape, i) for i in top_bin_centers]
+    bot_z = [geometry.unified_to_z(bot_mito_shape, i) for i in bot_bin_centers]
 
-    flat_max = np.sqrt(2 * (mito_shape.l_flat / 2) ** 2) - mito_shape.r_cylinder - mito_shape.r_junction
-    outer_flat_bins = np.arange(0, outer_flat_coord.max(), bin_spacing)
-    outer_flat_bin_assignments = np.digitize(outer_flat_coord, outer_flat_bins)
-    outer_flat_bin_centers = (outer_flat_bins[1:] + outer_flat_bins[:-1]) / 2
 
-    cyl_force_averages = np.zeros(len(outer_cyl_bins) - 1)
-    for bin_ind in range(1, len(outer_cyl_bins)):
-        force_ind = bin_ind - 1
-        cyl_force_averages[force_ind] = outer_cyl_force[outer_cyl_bin_assignments == bin_ind].mean()
 
-    junc_force_averages = np.zeros(len(outer_junc_bins) - 1)
-    for bin_ind in range(1, len(outer_junc_bins)):
-        force_ind = bin_ind - 1
-        junc_force_averages[force_ind] = outer_junc_force[outer_junc_bin_assignments == bin_ind].mean()
+    max_force = 0.0699768 * 4
 
-    flat_force_averages = np.zeros(len(outer_flat_bins) - 1)
-    for bin_ind in range(1, len(outer_flat_bins)):
-        force_ind = bin_ind - 1
-        flat_force_averages[force_ind] = outer_flat_force[outer_flat_bin_assignments == bin_ind].mean()
 
-    plt.scatter(dummy_forces)
+    popc_path = '/home/kevin/hdd/Projects/mito/simulations/dummy_frozen/POPC_100'
+    dope_path = '/home/kevin/hdd/Projects/mito/simulations/dummy_frozen/POPC80_DOPE20'
+    tocl_path = '/home/kevin/hdd/Projects/mito/simulations/dummy_frozen/POPC80_TOCL20'
+
+    mito_shape = geometry.mito_dims(30, 10, 10, 56)
+    firstframe = 2000
+    popc_data = process_dummy_system(popc_path, 6.5 / 2, mito_shape, firstframe=firstframe)
+    tocl_data = process_dummy_system(tocl_path, 6.5 / 2, mito_shape, firstframe=firstframe)
+    dope_data = process_dummy_system(dope_path, 6.5 / 2, mito_shape, firstframe=firstframe)
+
+    plt.figure()
+    plt.scatter(tocl_data.inner_rho, tocl_data.inner_z, c=tocl_data.inner_force * 4, cmap='Reds', vmin=0, vmax=max_force)
+    plt.scatter(tocl_data.outer_rho, tocl_data.outer_z, c=tocl_data.outer_force * 4, cmap='Reds', vmin=0, vmax=max_force)
+    plt.title('tocl')
+    plt.xlabel("rho (nm)")
+    plt.ylabel("z (nm)")
+    cbar = plt.colorbar()
+    cbar.set_label("pressure (kJ / (mol nm ^3))")
+
+    plt.figure()
+    plt.scatter(dope_data.inner_rho, dope_data.inner_z, c=dope_data.inner_force * 4, cmap='Reds', vmin=0, vmax=max_force)
+    plt.scatter(dope_data.outer_rho, dope_data.outer_z, c=dope_data.outer_force * 4, cmap='Reds', vmin=0, vmax=max_force)
+    plt.title('dope')
+    plt.xlabel("rho (nm)")
+    plt.ylabel("z (nm)")
+    cbar = plt.colorbar()
+    cbar.set_label("pressure (kJ / (mol nm ^3))")
+
+    plt.figure()
+    plt.scatter(popc_data.inner_rho, popc_data.inner_z, c=popc_data.inner_force * 4, cmap='Reds', vmin=0, vmax=max_force)
+    plt.scatter(popc_data.outer_rho, popc_data.outer_z, c=popc_data.outer_force * 4, cmap='Reds', vmin=0, vmax=max_force)
+    plt.title('popc')
+    plt.xlabel("rho (nm)")
+    plt.ylabel("z (nm)")
+    cbar = plt.colorbar()
+    cbar.set_label("pressure (kJ / (mol nm ^3))")
--- analysis/geometry.py
+++ analysis/geometry.py
@@ -10,7 +10,7 @@ class mito_dims:
         self.l_flat  = l_flat
 
 
-def get_mito_center(coords, dims):
+def get_mito_center(coords, l_cylinder):
     '''
         For the z center, can just take the midpoint of the maximum and minimum coordinates, as the top and bottom
         plane z values are regular. For x/y, we have to potentially account for e.g. a cutoff of dummies along the x or
@@ -26,12 +26,12 @@ def get_mito_center(coords, dims):
         Returns the center as a np.array(3), doesn't make transformation
     '''
     z_com = (coords[:, 2].max() - coords[:, 2].min()) / 2.0
-    xy_inrange = np.abs(coords[:, 2] - z_com) < dims.l_cylinder / 2
+    xy_inrange = np.abs(coords[:, 2] - z_com) < l_cylinder / 2
     xy_means = coords[xy_inrange, 0:2].mean(axis=0)
     return np.array((xy_means[0], xy_means[1], z_com))
 
 
-def assign_to_mito_section(rho, z, mito_dims):
+def assign_to_mito_section(rho, z, mito_shape):
     ''' For one frame, generates an assignment_indices instance assigning inner and outer indices to the three
         regions of the mitochondria
 
@@ -47,11 +47,12 @@ def assign_to_mito_section(rho, z, mito_dims):
             in_junction - list of indices. union of in_cylinder, in_flat, in_junction is 0 to rho.size - 1
 
     '''
-    in_cylinder = np.where(np.abs(z) < (mito_dims.l_cylinder / 2))[0]
-    in_flat     = np.where(rho > (mito_dims.r_cylinder + mito_dims.r_junction))[0]
-    in_junction = np.where((np.abs(z) >= (mito_dims.l_cylinder / 2)) &
-                           (rho <= (mito_dims.r_cylinder + mito_dims.r_junction)))[0]
-    return in_cylinder, in_flat, in_junction
+    in_cylinder = np.where(np.abs(z) < (mito_shape.l_cylinder / 2))[0]
+    in_junction = np.where((np.abs(z) >= (mito_shape.l_cylinder / 2)) &
+                           (rho <= (mito_shape.r_cylinder + mito_shape.r_junction)))[0]
+    in_flat     = np.where(rho > (mito_shape.r_cylinder + mito_shape.r_junction))[0]
+
+    return in_cylinder, in_junction, in_flat
 
 
 def map_to_cyl_regime(z, mito_shape):
@@ -78,8 +79,12 @@ def map_to_junction_regime(z, rho, mito_shape):
     return 180 - np.arctan2(centered_z, centered_rho) * 180 / np.pi
 
 
+def calc_inner_toroidal_area_by_angle(r_torus, r_tube, theta):
+    return 2 * np.pi * r_tube * r_torus * theta - 2 * np.pi * r_tube * r_tube * np.sin(theta)
+
+
 def map_to_unified_coordinate(z, rho, mito_shape):
-    in_cyl, in_flat, in_junc = assign_to_mito_section(rho, z, mito_shape)
+    in_cyl, in_junc, in_flat = assign_to_mito_section(rho, z, mito_shape)
 
     junc_offset = mito_shape.l_cylinder / 2
     flat_offset = junc_offset + np.pi * mito_shape.r_junction / 2
@@ -90,3 +95,106 @@ def map_to_unified_coordinate(z, rho, mito_shape):
     unified_coordinate[in_flat] = map_to_flat_regime(rho[in_flat], mito_shape) + flat_offset
 
     return unified_coordinate
+
+
+def circle_segment_area(r, t):
+    ''' r is circle radius, t is distance from center to perpindicular chord'''
+    return  (r ** 2) * np.arccos(t / r) - t * np.sqrt(r ** 2 - t ** 2)
+
+
+def unified_to_junction_angle(mito_dims, unified_coordinate):
+    arc_length = unified_coordinate - mito_dims.l_cylinder / 2
+    return arc_length / mito_dims.r_junction
+
+
+def unified_to_flat_radius(mito_dims, unified_coordinate):
+    return mito_dims.r_junction + mito_dims.r_cylinder + (unified_coordinate - mito_dims.l_cylinder / 2 - mito_dims.r_junction * np.pi / 2)
+
+
+def unified_to_rho(mito_dims, unified_coordinate):
+    rho = mito_dims.r_cylinder
+    if unified_coordinate > mito_dims.l_cylinder / 2:
+        if unified_coordinate <= mito_dims.l_cylinder / 2 + mito_dims.r_junction * np.pi / 2:
+            j_angle = unified_to_junction_angle(mito_dims, unified_coordinate)
+            rho += mito_dims.r_junction * (1 - np.cos(j_angle))
+        else:
+            rho = unified_to_flat_radius(mito_dims, unified_coordinate)
+    return rho
+
+
+def unified_to_z(mito_dims, unified_coordinate):
+    if unified_coordinate <= mito_dims.l_cylinder / 2:
+        z = unified_coordinate
+    elif unified_coordinate < mito_dims.l_cylinder / 2 + mito_dims.r_junction * np.pi / 2:
+        j_angle = unified_to_junction_angle(mito_dims, unified_coordinate)
+        z = mito_dims.l_cylinder / 2 + mito_dims.r_junction * np.sin(j_angle)
+    else:
+        z = mito_dims.l_cylinder / 2 + mito_dims.r_junction
+    return z
+
+
+def calc_cylindrical_section_area(mito_dims, bin_edge_1, bin_edge_2):
+    ''' Just takes simple difference in l dimension between edges '''
+    return 2 * np.pi * mito_dims.r_cylinder * (bin_edge_2 - bin_edge_1)
+
+
+def calc_junction_section_area(mito_dims, bin_angle_1, bin_angle_2):
+    ''' Calculate the two partial areas and subtract. Bin angles are 0 for the cylindrical end, pi/2 for the flat end'''
+    r_torus = mito_dims.r_cylinder + mito_dims.r_junction
+    r_tube = mito_dims.r_junction
+    return calc_inner_toroidal_area_by_angle(r_torus, r_tube, bin_angle_2) - calc_inner_toroidal_area_by_angle(r_torus, r_tube, bin_angle_1)
+
+
+def calc_flat_section_area(mito_dims, bin_r_1, bin_r_2):
+    ''' Bin inputs should be radii'''
+    if bin_r_1 > mito_dims.l_flat / 2:
+        coord1_excluded = 4 * circle_segment_area(bin_r_1, mito_dims.l_flat / 2)
+    else:
+        coord1_excluded = 0
+
+    if bin_r_2 > mito_dims.l_flat / 2:
+        coord2_excluded = 4 * circle_segment_area(bin_r_2, mito_dims.l_flat / 2)
+    else:
+        coord2_excluded = 0
+
+    return (np.pi * (bin_r_2 ** 2) - coord2_excluded) - (np.pi * (bin_r_1 ** 2) - coord1_excluded)
+
+
+def get_unified_coord_section(mito_dims, unified_coordinate):
+    if unified_coordinate <= mito_dims.l_cylinder / 2:
+        return 'cyl'
+    elif unified_coordinate >= mito_dims.l_cylinder / 2 + np.pi * mito_dims.r_junction  / 2:
+        return 'flat'
+    else:
+        return 'junc'
+
+
+def calc_unified_section_area(mito_dims, coord1, coord2):
+    coord1_section = get_unified_coord_section(mito_dims, coord1)
+    coord2_section = get_unified_coord_section(mito_dims, coord2)
+
+    if coord1_section == 'cyl':
+        if coord2_section == 'cyl':
+            return calc_cylindrical_section_area(mito_dims, coord1, coord2)
+        elif coord2_section == 'junc':
+            # get cylindrical area up to junction border, then junction area from 0 to coord 2
+            cyl_region = calc_cylindrical_section_area(mito_dims, coord1, mito_dims.l_cylinder / 2)
+            coord2_angle = unified_to_junction_angle(mito_dims, coord2)
+            return cyl_region + calc_junction_section_area(mito_dims, 0, coord2_angle)
+        else:
+            raise NotImplementedError("cyl to flat area not implemented")
+    elif coord1_section == 'junc':
+        coord1_angle = unified_to_junction_angle(mito_dims, coord1)
+        if coord2_section == 'junc':
+            coord2_angle = unified_to_junction_angle(mito_dims, coord2)
+            return calc_junction_section_area(mito_dims, coord1_angle, coord2_angle)
+        elif coord2_section == 'flat':
+            # calculate junction area to flat border
+            junc_area = calc_junction_section_area(mito_dims, coord1_angle, np.pi / 2)
+            # add flat area from border, which is r_junc + r_cyl
+            coord2_radius = unified_to_flat_radius(mito_dims, coord2)
+            return junc_area + calc_flat_section_area(mito_dims, mito_dims.r_junction + mito_dims.r_cylinder, coord2_radius)
+    elif coord1_section == 'flat':
+        return calc_flat_section_area(mito_dims, unified_to_flat_radius(mito_dims, coord1), unified_to_flat_radius(mito_dims, coord2))
+    else:
+        raise Exception("coordinate section not defined")
